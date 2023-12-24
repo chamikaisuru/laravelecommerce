58414913defec822cdd91a8b91c37ee44fcd8677
chamikaisuru <isurumadusanka.tmc@gmail.com>
2023-12-24 17:27:58 +0530

    make seeder for product table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         //$this->call(AdminsTableSeeder::class);
-        $this->call(categoriesTableSeeder::class);
+        //$this->call(categoriesTableSeeder::class);
+        $this->call(ProductTableSedeer::class);
         //$this->call(SubAdminSeeder::class);
         //$this->call(cmspagetableseeder::class);
     }
